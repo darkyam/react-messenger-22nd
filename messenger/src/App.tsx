225caf8a0c5ba8660deb0778e65dbe4fac87b97f
yam <1669jsh@naver.com>
2025-10-24 23:00:07 +0900

    feat: 라우팅 설정

--- messenger/src/App.tsx
+++ messenger/src/App.tsx
@@ -1,43 +1,53 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import type { Room, User, Message } from './assets/type';
 import ChatRoom from './pages/chat/chatRoom';
+import Home from '@pages/home/home';
+import Chat from '@pages/chat/chat';
 
-// 더미 유저
-const me: User = { id: 'me', name: '나' };
-const userA: User = { id: 'u1', name: '김건우' };
-const userB: User = { id: 'u2', name: '조예원' };
+import usersData from '@assets/data/user.json';
+import roomsData from '@assets/data/room.json';
 
-// 더미 메시지
-const dummyMessages: Message[] = [
-  {
-    id: 'm1',
-    sender: userA,
-    text: '안녕?',
-    timestamp: Date.now() - 50000000000,
-    chatRoomId: 'room1',
-  },
-  {
-    id: 'm2',
-    sender: userB,
-    text: '오늘 뭐해?',
-    timestamp: Date.now() - 5000,
-    chatRoomId: 'room1',
-  },
-];
-
-// 더미 채팅방
-const dummyRoom: Room = {
-  id: 'room1',
-  name: 'Offnal v2',
-  members: [me, userA, userB],
-  messages: dummyMessages,
-};
+import type { RawRoom, Room, User } from '@assets/type';
+import { useState } from 'react';
 
 function App() {
+  const me = usersData.find((u) => u.id === 'me') as User;
+  const friendsList = usersData.filter((u) => u.id !== 'me') as User[];
+
+  const userMap = Object.fromEntries(usersData.map((u: User) => [u.id, u]));
+
+  const [rooms, setRooms] = useState<Room[]>(() => {
+    return (roomsData as RawRoom[]).map((r) => {
+      const savedMessages = localStorage.getItem(`chat_messages_${r.id}`);
+      return {
+        id: r.id,
+        name: r.name,
+        members: r.memberIds.map((id) => userMap[id]),
+        messages: savedMessages
+          ? JSON.parse(savedMessages)
+          : r.messages.map((m) => ({
+              id: m.id,
+              sender: userMap[m.senderId],
+              text: m.text,
+              timestamp: m.timestamp,
+              chatRoomId: r.id,
+            })),
+      };
+    });
+  });
+
   return (
-    <>
-      <ChatRoom room={dummyRoom} me={me} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home me={me} friendsList={friendsList} />} />
+        <Route path="/chat" element={<Chat rooms={rooms} />} />
+        <Route
+          path="/chat/:roomId"
+          element={<ChatRoom rooms={rooms} me={me} setRooms={setRooms} />}
+        />
+        {/* <ChatRoom room={dummyRoom} me={me} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
